1b885bd1e55cc67c48802526b6fe7d626c6744f0
dodiameer <mtxshiftg@gmail.com>
2021-04-25 02:11:49 +0300

    chore: enabled SW

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import App from "./App.svelte";
 import "$assets/global.css";
+// @ts-ignore
+import { registerSW } from "virtual:pwa-register";
 
 const app = new App({
   target: document.getElementById("app"),
 });
+registerSW();
 
 export default app;
